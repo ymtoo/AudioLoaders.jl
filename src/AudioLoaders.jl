e8c394ab9716ba28549ebe9f37b9e068f141ad02
ymtoo <ymtoo86@gmail.com>
2021-11-30 18:36:28 +0800

    feat: add device for embed

--- src/AudioLoaders.jl
+++ src/AudioLoaders.jl
@@ -3,7 +3,7 @@ module AudioLoaders
 using Reexport
 
 @reexport using DSP: Windows, stft, power, pow2db
-using Flux: Data._nobs, Data._getobs
+using Flux: Data._nobs, Data._getobs, cpu, gpu
 using ImageTransformations
 using Random: AbstractRNG, shuffle!, GLOBAL_RNG
 using WAV
--- src/embeddings.jl
+++ src/embeddings.jl
@@ -1,17 +1,17 @@
 export embed
 
-function embed(f::Function, d::AudioLoader)
+function embed(f::Function, d::AudioLoader; device::Function=cpu)
     n = length(first(d.data))
-    z = f(unpack_data(d.config, first(d))...)
+    z = f(device.(unpack_data(d.config, first(d)))...)
     m, batchsize = size(z)
     Z = zeros(sampletype, m, n)
     Z[:,1:batchsize] = z
     for (i, d1) ∈ enumerate(d)
         if i > 1
-            z = f(unpack_data(d.config, d1)...)
+            z = f(device.(unpack_data(d.config, d1))...)
             startindex = (i - 1) * batchsize + 1
             stopindex = startindex + size(z, 2) - 1
-            Z[:,startindex:stopindex] = z
+            Z[:,startindex:stopindex] = cpu(z) 
         end
     end
     Z
