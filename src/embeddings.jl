dcd2d2c062ba1e06e62d41455ba6b3c3db49f24f
ymtoo <ymtoo86@gmail.com>
2021-12-16 11:15:22 +0800

    fix: use todevice to avoid naming conflict

--- src/embeddings.jl
+++ src/embeddings.jl
@@ -1,15 +1,15 @@
 export embed
 
-function embed(f::Function, d::AudioLoader; device::Function=cpu, showprogress::Bool=false)
+function embed(f::Function, d::AudioLoader; todevice::Function=cpu, showprogress::Bool=false)
     p = Progress(length(d); enabled=showprogress)
     n = length(first(d.data))
-    z = f(device.(unpack_data(d.config, first(d)))...)
+    z = f(todevice.(unpack_data(d.config, first(d)))...)
     m, batchsize = size(z)
     Z = zeros(sampletype, m, n)
     Z[:,1:batchsize] = cpu(z)
     for (i, d1) ∈ enumerate(d)
         if i > 1
-            z = f(device.(unpack_data(d.config, d1))...)
+            z = f(todevice.(unpack_data(d.config, d1))...)
             startindex = (i - 1) * batchsize + 1
             stopindex = startindex + size(z, 2) - 1
             Z[:,startindex:stopindex] = cpu(z) 
