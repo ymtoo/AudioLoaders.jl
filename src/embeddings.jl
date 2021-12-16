6c08d30e5e839b23d2f34f5a6592b6e7f98a5e40
ymtoo <ymtoo86@gmail.com>
2021-12-16 15:32:49 +0800

    feat(embeddings): withtimesec and withsamplingrate

--- src/embeddings.jl
+++ src/embeddings.jl
@@ -1,15 +1,20 @@
 export embed
 
-function embed(f::Function, d::AudioLoader; todevice::Function=cpu, showprogress::Bool=false)
+function embed(f::Function, 
+               d::AudioLoader; 
+               withtimesec::Bool=true,
+               withsamplingrate::Bool=true, 
+               todevice::Function=cpu, 
+               showprogress::Bool=false)
     p = Progress(length(d); enabled=showprogress)
     n = length(first(d.data))
-    z = f(todevice.(unpack_data(d.config, first(d)))...)
+    z = f(todevice.(unpack_data(first(d), withtimesec, withsamplingrate))...)
     m, batchsize = size(z)
     Z = zeros(sampletype, m, n)
     Z[:,1:batchsize] = cpu(z)
     for (i, d1) ∈ enumerate(d)
         if i > 1
-            z = f(todevice.(unpack_data(d.config, d1))...)
+            z = f(todevice.(unpack_data(d1, withtimesec, withsamplingrate))...)
             startindex = (i - 1) * batchsize + 1
             stopindex = startindex + size(z, 2) - 1
             Z[:,startindex:stopindex] = cpu(z) 
